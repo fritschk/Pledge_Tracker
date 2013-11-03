2dbcf51094677e625331b542b2246821da7627e1
AJ <agumonaj@aol.com>
2013-11-03 07:52:29 -0500

    Did a fourth thing!

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,15 +1,15 @@
 class Post < ActiveRecord::Base
-  attr_accessible :activity, :activity_unit, :event, :name, :password, :username
+  attr_accessible :activity, :activity_unit, :event, :name
   
-  belongs_to :user
+  has_one :user
   
-  validates :username,  :presence => true
-  validates :password,  :presence => true
   validates :name,  :presence => true
   validates :event,  :presence => true
   validates :activity,  :presence => true
   validates :activity_unit,  :presence => true
   #vaidates_uniqueness_of :name
+  
+  
   
  #def set_date foo, bar
 #	foo
--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :post_attributes 
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :post_attributes  
   accepts_nested_attributes_for :post
   after_initialize :build_post, 
   unless: -> { post.present? } 
